b6af784144b499a6dcf933d1ef7c14ddb3a361a4
Dbenitez <diegobenitez@live.com.mx>
2020-07-23 03:22:54 -0500

    Cambios: En escoger lugar para conf, se muestra el nombre y la ubicacion a la vez

--- vistas/mod_conf_p.php
+++ vistas/mod_conf_p.php
@@ -95,11 +95,11 @@ $consulta = $cons->consultarPre($_GET['id']);
             <br>
             <h3 for="lugar">Lugar</h3>
             <select name="lugar" id="lugar">
-              <option value="<?php echo $consulta[6];?>"><?php echo $presencial->getNombreByID($consulta[6]);?></option>
+              <option value="<?php echo $consulta[6];?>"><?php echo $presencial->getUbicacionTabla($consulta[6]) . ", " . $presencial->getNombreByID($consulta[6]);?></option>
               <?php 
               foreach($arrayList as $nombre) {
               ?>
-              <option value="<?php echo $nombre['id_lugar'];?>"><?php echo $nombre['nombre'];?></option>
+              <option value="<?php echo $nombre['id_lugar'];?>"><?php echo $nombre['ubicacion'] . ", " . $nombre['nombre'];?></option>
               <?php
               }
               ?>
